114da8d1313774af61fb75d32eb798ae854f8a3b
Dowland Aiello <dowlandaiello@gmail.com>
2022-06-28 17:03:44 -0400

    Fix dependency misref after migration to service workers.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -1,7 +1,7 @@
 import { DEPLOYED_CONTRACTS } from "../utils/conf";
 import { METAMASK_SITE, ERR_NO_TEMPLATES } from "../utils/common";
 import { addAndChangeNetwork, login } from "../utils/eth";
-import { IPFSClient } from "../utils/ipfs";
+import { IPFSClient } from "ipfs-message-port-client";
 
 import LogoSquare from "../components/basic/LogoSquare";
 import { ActionableDialogue, DialogueStyle, DialogueComponent } from "../components/basic/ActionableDialogue";
